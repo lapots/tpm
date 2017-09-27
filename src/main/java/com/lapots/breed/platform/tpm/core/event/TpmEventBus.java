51fb9eec7dbe3fcd2bce951a2dbca8192b040c3f
lapots <sebastrident@gmail.com>
2017-09-27 20:35:55 +0300

    Adding registration of additional processors

--- src/main/java/com/lapots/breed/platform/tpm/core/event/TpmEventBus.java
+++ src/main/java/com/lapots/breed/platform/tpm/core/event/TpmEventBus.java
@@ -2,7 +2,9 @@ package com.lapots.breed.platform.tpm.core.event;
 
 import com.google.common.eventbus.EventBus;
 import com.lapots.breed.platform.tpm.core.api.event.TpmEvent;
+import com.lapots.breed.platform.tpm.core.event.processors.DownloadProcessor;
 import com.lapots.breed.platform.tpm.core.event.processors.ErrorProcessor;
+import com.lapots.breed.platform.tpm.core.event.processors.InstallationProcessor;
 import com.lapots.breed.platform.tpm.core.event.processors.LogNotifyProcessor;
 
 public enum TpmEventBus {
@@ -18,6 +20,8 @@ public enum TpmEventBus {
         TpmInternalEventBus() {
             register(new LogNotifyProcessor());
             register(new ErrorProcessor());
+            register(new DownloadProcessor());
+            register(new InstallationProcessor());
         }
     }
 }
