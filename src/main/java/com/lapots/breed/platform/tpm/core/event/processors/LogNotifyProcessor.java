1844c3ffdaa93e247316d769f34f2a7fa66ba10f
lapots <sebastrident@gmail.com>
2017-09-27 16:25:24 +0300

    Removed TODO for abstract subscribing as it doesn't work

--- src/main/java/com/lapots/breed/platform/tpm/core/event/processors/LogNotifyProcessor.java
+++ src/main/java/com/lapots/breed/platform/tpm/core/event/processors/LogNotifyProcessor.java
@@ -5,7 +5,7 @@ import com.lapots.breed.platform.tpm.core.api.event.IEventProcessor;
 import com.lapots.breed.platform.tpm.core.event.type.LogNotifyEvent;
 
 public class LogNotifyProcessor implements IEventProcessor<LogNotifyEvent> {
-    @Subscribe // TODO: investigate abstract
+    @Subscribe
     @Override
     public void processEvent(LogNotifyEvent event) {
         System.out.println(event.getMetadata());
